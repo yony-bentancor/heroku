458fa48bbe8339a40659d85e12072b5cad705dda
yony-bentancor <yonybentancor@gmail.com>
2022-11-15 09:41:41 -0300

    se cambio el nombre de archivo index por app

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const router = express.Router();
+
 const PORT = process.env.PORT || 8080;
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname + "/home.html")));
+
 app.listen(PORT, () => console.log("Example app listening on port 3000!"));
